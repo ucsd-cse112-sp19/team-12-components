0ea97cb7c1ec20ddda2ad1e5f5bbfa2b6445c20d
yiy142 <yiy142@ucsd.edu>
2019-05-11 13:57:14 -0700

    Init

---
+++ inputNum/main.js
@@ -0,0 +1,42 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    button, p {
+      display: inline-block;
+    }
+  </style>
+  <button aria-label="decrement">-</button>
+    <input type="number" value = 0></input>
+  <button aria-label="increment">+</button>
+`;
+
+class InputNum extends HTMLElement {
+  set value(value) {
+    this._value = value;
+    this.valueElement.value = this._value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  constructor() {
+    super();
+    this._value = 0;
+
+    this.root = this.attachShadow({ mode: 'open' });
+    this.root.appendChild(template.content.cloneNode(true));
+
+    this.valueElement = this.root.querySelector('input');
+    this.incrementButton = this.root.querySelectorAll('button')[1];
+    this.decrementButton = this.root.querySelectorAll('button')[0];
+
+    this.incrementButton
+      .addEventListener('click', (e) => this.value++);
+
+    this.decrementButton
+      .addEventListener('click', (e) => this.value--);
+  }
+}
+
+customElements.define('input-number', InputNum);
