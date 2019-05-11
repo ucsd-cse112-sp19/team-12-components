1ef53e3862181486c1da24387e8986634aaa6b58
ShayaP <shayaparsa6@gmail.com>
2019-05-11 16:21:51 -0700

    added styles for the input number

--- inputNum/inputNumber.js
+++ inputNum/inputNumber.js
@@ -1,10 +1,10 @@
 const template = document.createElement('template');
 template.innerHTML = `
   <link rel="stylesheet" href="inputNumber.css">
-  <div class="input-number">
-    <button aria-label="decrement" class="decrement-btn">-</button>
-    <input type="number" value = 0 class="input-field"></input>
-    <button aria-label="increment" class="increment-btn">+</button>
+  <div class="input-number large">
+    <button aria-label="decrement" class="decrement-btn">-</button><!--
+    --><input type="number" value = 0 class="input-field"></input><!--
+    --><button aria-label="increment" class="increment-btn">+</button>
   </div>
 `;
 
@@ -25,6 +25,7 @@ class InputNum extends HTMLElement {
     this.root = this.attachShadow({ mode: 'open' });
     this.root.appendChild(template.content.cloneNode(true));
 
+    this.inputDiv = this.root.querySelector('div');
     this.valueElement = this.root.querySelector('input');
     this.incrementButton = this.root.querySelectorAll('button')[1];
     this.decrementButton = this.root.querySelectorAll('button')[0];
@@ -36,7 +37,32 @@ class InputNum extends HTMLElement {
       .addEventListener('click', (e) => this.value--);
 
     this.valueElement
-      .addEventListener('keyup', (e) => this.value = this.valueElement.value)
+      .addEventListener('keyup', (e) => this.value = this.valueElement.value);
+
+    this.decrementButton
+      .addEventListener('mouseover', (e) => {
+        this.inputDiv.style.borderColor = "#75baff";
+        this.decrementButton.style.color = "#75baff";
+      });
+    this.decrementButton
+      .addEventListener('mouseout', (e) => {
+        this.inputDiv.style.borderColor = "#c2c2c2";
+        this.decrementButton.style.color = "black";
+      });
+
+    this.incrementButton
+      .addEventListener('mouseover', (e) => {
+        this.inputDiv.style.borderColor = "#75baff";
+        this.incrementButton.style.color = "#75baff";
+      });
+    this.incrementButton
+      .addEventListener('mouseout', (e) => {
+        this.inputDiv.style.borderColor = "#c2c2c2";
+        this.incrementButton.style.color = "black";
+      });
+
+    this.valueElement
+      .addEventListener('click', (e) => this.inputDiv.style.borderColor = "#75baff");
   }
 }
 
