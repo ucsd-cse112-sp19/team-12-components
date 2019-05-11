d220d9c8b06e8d4d0b626b09363950ebf9254701
yiy142 <yiy142@ucsd.edu>
2019-05-11 16:26:00 -0700

    attributes

--- inputNum/inputNumber.js
+++ inputNum/inputNumber.js
@@ -1,25 +1,114 @@
 const template = document.createElement('template');
 template.innerHTML = `
-  <link rel="stylesheet" href="inputNumber.css">
+  <style>
+    button, p {
+      display: inline-block;
+    }
+    input[type="number"] {
+      -webkit-appearance: textfield;
+         -moz-appearance: textfield;
+              appearance: textfield;
+    }
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button { 
+      -webkit-appearance: none;
+    }
+  </style>
   <button aria-label="decrement">-</button>
-    <input type="number" value = 0></input>
+    <input type="number"></input>
   <button aria-label="increment">+</button>
 `;
 
 class InputNum extends HTMLElement {
   set value(value) {
     this._value = value;
-    this.valueElement.value = this._value;
+    this.valueElement.value = this.value;
   }
-
   get value() {
     return this._value;
   }
 
+  set size(sizeValue){
+    this._size = sizeValue;
+  }
+  get size(){
+    return this._size;
+  }
+
+  set step(stepValue){
+    this._step = stepValue;
+  }
+  get step(){
+    return this._step;
+  }
+
+  set disabled(isdisabled){
+    this.setAttribute
+  }
+
+  static get observedAttributes() { return [ 'min', 'max', 'step', 'size', 'disabled', 'placeholder', 'value']; }
+  
+  connectedCallback() {
+    if (!this.hasAttribute('min'))
+      this.valueElement.min = Number.NEGATIVE_INFINITY;
+    
+    if (!this.hasAttribute('max'))
+      this.valueElement.max = Number.POSITIVE_INFINITY;
+    
+    if (!this.hasAttribute('step'))
+      this.step = 1;
+    
+    if(!this.hasAttribute('size'))
+      //TODO
+      this.valueElement.setAttribute('size','small');
+  
+    if(!this.hasAttribute('disabled'))
+      //TODO
+    
+    /*
+    if(!this.hasAttribute('placeholder'))
+      this.valueElement.setAttribute('placeholder', '');
+    */
+    if (!this.hasAttribute('value'))
+      this.value = 0;
+    
+    
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    switch (attrName) {
+      case 'min':
+        this.valueElement.setAttribute(min,parseInt(newValue, 10));
+        //this.valueElement.min = parseInt(newValue, 10);
+        break;
+      case 'max':
+        this.valueElement.max = parseInt(newValue, 10);
+        break;
+      case 'step':
+        this.step = parseInt(newValue, 10);
+        break;
+      case 'size':
+        //TODO
+        break;
+      case 'disabled':
+        //TODO
+        break;
+      case 'placeholder':
+        this.valueElement.setAttribute('placeholder',newValue);
+        break;
+      case 'value':
+        this.value = parseInt(newValue, 10);
+        break;
+      case 'controls':
+        if (newValue === 'true'){
+          this.incrementButton.style.display = 'none';
+          this.decrementButton.style.display = 'none';
+        }
+      }
+  }
+
   constructor() {
     super();
-    this._value = 0;
-
     this.root = this.attachShadow({ mode: 'open' });
     this.root.appendChild(template.content.cloneNode(true));
 
@@ -28,13 +117,24 @@ class InputNum extends HTMLElement {
     this.decrementButton = this.root.querySelectorAll('button')[0];
 
     this.incrementButton
-      .addEventListener('click', (e) => this.value++);
+      .addEventListener('click', (e) => {
+        console.log(this.step);
+        if (this.valueElement.max > this.value+this.step)
+          this.value+=this.step;
+        else
+          window.alert("Number too big");
+      });
 
     this.decrementButton
-      .addEventListener('click', (e) => this.value--);
+      .addEventListener('click', (e) => {
+        if (this.valueElement.min < this.value-this.step)
+          this.value-=this.step;
+        else
+          window.alert("Number too small")
+      });
 
     this.valueElement
-      .addEventListener('keyup', (e) => this.value = this.valueElement.value)
+      .addEventListener('keyup', (e) => this.value = this.valueElement.value);
   }
 }
 
