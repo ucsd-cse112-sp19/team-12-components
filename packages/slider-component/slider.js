1fcdf0ebfb93876a1f276758e03e8d0967c90494
Martina Lofqvist <martinalofqvist@outlook.com>
2019-05-12 13:29:10 -0700

    clean up slider js

--- packages/slider-component/slider.js
+++ packages/slider-component/slider.js
@@ -1,28 +1,11 @@
 var Main = {
-    data() {
-      return {
-        value1: 0,
-        value2: 50,
-        value3: 36,
-        value4: 48,
-        value5: 42,
-        value6: 0,
-        value7: 0
-      }
-    },
-    methods: {
-      formatTooltip(val) {
-        return val / 100;
-      }
+  data() {
+    return {
+      value1: 0, value2: 50, value3: 36, value4: 48, value5: 42, value6: 0,
+          value7: 0
     }
-  }
-var Ctor = Vue.extend(Main)
+  },
+  methods : {formatTooltip(val) { return val / 100; }}
+} 
+var Ctor = Vue.extend(Main) 
 new Ctor().$mount('#app')
-
-
-// new Vue({
-//    el: '#app',
-//    data: function() {
-//      return { visible: false }
-//    }
-//  })
