efa6f1345c1ea124cefc419f84bb7611b4ce36c0
Martina Lofqvist <martinalofqvist@outlook.com>
2019-05-12 13:44:19 -0700

    finished slider implementation

--- packages/slider-component/slider.js
+++ packages/slider-component/slider.js
@@ -2,7 +2,18 @@ var Main = {
   data() {
     return {
       value1: 0, value2: 50, value3: 36, value4: 48, value5: 42, value6: 0,
-          value7: 0
+          value7: 0, value8: 0, value9: 0, value10: 0, value11: [30, 60],
+          marks: {
+            0: '0°C',
+            8: '8°C',
+            37: '37°C',
+            50: {
+              style: {
+                color: '#1989FA'
+              },
+              label: this.$createElement('strong', '50%')
+            }
+          }
     }
   },
   methods : {formatTooltip(val) { return val / 100; }}
