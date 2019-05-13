cff602cec3fd2c670dd64515db85d5a7ce27aa6a
Martina Lofqvist <martinalofqvist@outlook.com>
2019-05-13 16:04:03 -0700

    vanilla slider

--- packages/slider-component/slider.js
+++ packages/slider-component/slider.js
@@ -2,15 +2,13 @@ var Main = {
   data() {
     return {
       value1: 0, value2: 50, value3: 36, value4: 48, value5: 42, value6: 0,
-          value7: 0, value8: 0, value9: 0, value10: 0, value11: [30, 60],
+          value7: 0, value8: 0, value9: 0, value10: 0, value11: [ 30, 60 ],
           marks: {
             0: '0°C',
             8: '8°C',
             37: '37°C',
             50: {
-              style: {
-                color: '#1989FA'
-              },
+              style: {color: '#1989FA'},
               label: this.$createElement('strong', '50%')
             }
           }
