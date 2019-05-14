60252ae65161ef86470903560650ffde0c7654bc
Martina Lofqvist <martinalofqvist@outlook.com>
2019-05-14 16:51:54 -0700

    implementing event listeners

--- packages/slider-component/vanilla-slider-v2/vanilla-slider-v2.js
+++ packages/slider-component/vanilla-slider-v2/vanilla-slider-v2.js
@@ -6,8 +6,10 @@ template.innerHTML = `
     <div role="slider" aria-valuemin="0" aria-valuemax="100" aria-orientation="horizontal" class="el-slider" aria-valuetext="0" aria-label="slider between 0 and 100">
         <div class="el-slider__runway">
             <div class="el-slider__bar" style="width: 25%; left: 0%;"></div>
+            <!--
             <div tabindex="0" class="el-slider__button-wrapper" style="left: 25%;">
                 <div class="el-tooltip el-slider__button" aria-describedby="el-tooltip-9861" tabindex="0"></div>
+            -->
             </div>
         </div>
     </div>
@@ -20,45 +22,57 @@ class VanillaSliderV2 extends HTMLElement {
         this.root.appendChild(template.content.cloneNode(true));
 
         this.sliderContainer = this.root.querySelector('.el-slider');
+        this.sliderRunway = this.root.querySelector('.el-slider__runway');
         this.sliderBar = this.root.querySelector('.el-slider__bar');
         this.sliderBtnWrapper = this.root.querySelector('.el-slider__button-wrapper');
         this.tootip = this.root.querySelector('.el-tooltip.el-slider__button');
 
         console.log(this.sliderBar.style.width);
+
+        // Bind the "this" to the functions
+        this.onButtonDown = this.onButtonDown.bind(this);
+        this.onDragStart = this.onDragStart.bind(this);
     }
 
     connectedCallback() {
-        // this.sliderBar.addEventListener('')
+        //this.sliderBar.addEventListener('', this.setPosition(value));
+        // Bind event listener to sliderbar whenever it is clicked
+        this.sliderRunway.addEventListener('click', this.onButtonDown);
     }
 
     // TODO: implement event handlers
 
-    // onButtonDown(event) {
-    //     if (this.disabled) return;
-    //     event.preventDefault();
-    //     this.onDragStart(event);
-    //     window.addEventListener('mousemove', this.onDragging);
-    //     window.addEventListener('touchmove', this.onDragging);
-    //     window.addEventListener('mouseup', this.onDragEnd);
-    //     window.addEventListener('touchend', this.onDragEnd);
-    //     window.addEventListener('contextmenu', this.onDragEnd);
-    // }
+    onButtonDown(event) {
+        if (this.disabled) return;
+        event.preventDefault();
+        this.onDragStart(event);
+        window.addEventListener('mousemove', this.onDragging);
+        window.addEventListener('touchmove', this.onDragging);
+        window.addEventListener('mouseup', this.onDragEnd);
+        window.addEventListener('touchend', this.onDragEnd);
+        window.addEventListener('contextmenu', this.onDragEnd);
+        console.log(event);
+    };
 
-    // onDragStart(event) {
-    //     this.dragging = true;
-    //     this.isClick = true;
-    //     if (event.type === 'touchstart') {
-    //         event.clientY = event.touches[0].clientY;
-    //         event.clientX = event.touches[0].clientX;
-    //     }
-    //     if (this.vertical) {
-    //         this.startY = event.clientY;
-    //     } else {
-    //         this.startX = event.clientX;
-    //     }
-    //     this.startPosition = parseFloat(this.currentPosition);
-    //     this.newPosition = this.startPosition;
-    // }
+    onDragStart(event) {
+        this.dragging = true;
+        this.isClick = true;
+        console.log(event.clientX);
+        if (event.type === 'touchstart') {
+            // event.clientY = event.touches[0].clientY;
+            event.clientX = event.touches[0].clientX;
+        }
+        if (this.vertical) {
+            // this.startY = event.clientY;
+        } else {
+            this.startX = event.clientX;
+        }
+        this.currentPosition = this.sliderBar.style.width;
+        this.startPosition = parseFloat(this.currentPosition);
+        this.newPosition = this.startPosition;
+        console.log(this.startPosition);
+        console.log(this.newPosition);
+    };
 
     // onDragging(event) {
     //     if (this.dragging) {
@@ -100,25 +114,42 @@ class VanillaSliderV2 extends HTMLElement {
     //     }
     // }
 
+    // Set the position of the drop being dragged
     // setPosition(newPosition) {
     //     if (newPosition === null || isNaN(newPosition)) return;
-    //     if (newPosition < 0) {
-    //         newPosition = 0;
-    //     } else if (newPosition > 100) {
-    //         newPosition = 100;
+    //        if (newPosition < 0) {
+    //          newPosition = 0;
+    //      } else if (newPosition > 100) {
+    //          newPosition = 100;
+    //      }
+    //      const lengthPerStep = 100 / ((this.max - this.min) / this.step);
+    //      const steps = Math.round(newPosition / lengthPerStep);
+    //      let value = steps * lengthPerStep * (this.max - this.min) * 0.01 + this.min;
+    //      value = parseFloat(value.toFixed(this.precision));
+    //      this.$emit('input', value);
+    //      this.$nextTick(() => {
+    //          this.$refs.tooltip && this.$refs.tooltip.updatePopper();
+    //      });
+    //      if (!this.dragging && this.value !== this.oldValue) {
+    //          this.oldValue = this.value;
+    //      }
+    //  }
+
+    //  // Call the set position 
+    //  setPosition(percent) {
+    //     const targetValue = this.min + percent * (this.max - this.min) / 100;
+    //     if (!this.range) {
+    //       this.$refs.button1.setPosition(percent);
+    //       return;
     //     }
-    //     const lengthPerStep = 100 / ((this.max - this.min) / this.step);
-    //     const steps = Math.round(newPosition / lengthPerStep);
-    //     let value = steps * lengthPerStep * (this.max - this.min) * 0.01 + this.min;
-    //     value = parseFloat(value.toFixed(this.precision));
-    //     this.$emit('input', value);
-    //     this.$nextTick(() => {
-    //         this.$refs.tooltip && this.$refs.tooltip.updatePopper();
-    //     });
-    //     if (!this.dragging && this.value !== this.oldValue) {
-    //         this.oldValue = this.value;
+    //     let button;
+    //     if (Math.abs(this.minValue - targetValue) < Math.abs(this.maxValue - targetValue)) {
+    //       button = this.firstValue < this.secondValue ? 'button1' : 'button2';
+    //     } else {
+    //       button = this.firstValue > this.secondValue ? 'button1' : 'button2';
     //     }
-    // }
+    //     this.$refs[button].setPosition(percent);
+    //   }
 
     static get observedAttributes() {
         return ['value', 'min', 'max'];
