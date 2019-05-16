2e40d9c7e9b039159b261e6ac975e79499d9c29b
ms <mis046@ucsd.edu>
2019-05-15 22:05:22 -0700

    Simple UI tests for hello world component
    - Test if texts are displayed correctly in different settings
    - Test in both Chrome and Firefox
    - Safari has weird issue, see slack

--- test/core-hello/core-hello.ui_test.js
+++ test/core-hello/core-hello.ui_test.js
@@ -1,22 +1,19 @@
+// TestCafe APIs could be found here:
+// https://devexpress.github.io/testcafe/documentation/test-api/
+
 import { Selector } from "testcafe";
 
-fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
+fixture`Getting Started`.page`../../packages/core-hello/demo/core_hello.html`;
 
-test("My first test", async t => {
-  await t
-    .typeText("#developer-name", "John Smith")
-    .click("#submit-button")
-
+test("Example Test for core-hello: test if texts are correctly displayed", async t => {
     // Use the assertion to check if the actual header text is equal to the expected one
-    .expect(Selector("#article-header").innerText)
-    .eql("Thank you, John Smith!");
-});
-
-test("My second test", async t => {
-  await t.typeText("#developer-name", "John Smith").click("#submit-button");
-
-  const articleHeader = await Selector(".result-content").find("h1");
-
-  // Obtain the text of the article header
-  let headerText = await articleHeader.innerText;
+    const component_sp = Selector('#sp');
+    const component_en = Selector('#en');
+    const component_fr = Selector('#fr');
+    let text_sp = await component_sp.innerText;
+    let text_en = await component_en.innerText;
+    let text_fr = await component_fr.innerText;
+    await t.expect(component_sp.innerText).eql('Hola Mundo Shaya!');
+    await t.expect(component_en.innerText).eql('Hello World Shaya!');
+    await t.expect(component_fr.innerText).eql('Salut Monde Shaya!');
 });
