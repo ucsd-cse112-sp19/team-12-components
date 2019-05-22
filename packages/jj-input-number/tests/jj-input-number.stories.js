31681012e5b4d7799695f4d41cfcb3aad7d0e2b3
Nate <nate@objectsecurity.com>
2019-05-22 15:31:40 -0700

    fix some visual tests

--- packages/jj-input-number/tests/jj-input-number.stories.js
+++ packages/jj-input-number/tests/jj-input-number.stories.js
@@ -2,5 +2,5 @@ import {storiesOf} from '@storybook/html';
 import '../jj-input-number.js';
 
 storiesOf('jj-input-number', module)
-  .add('Default', () => 
-     '<jj-slider value=35 min=0 max=50 color="purple"></jj-slider>');
+  .add('min, max, start value', () =>
+     '<jj-input-number min=0 max=8 value=1 precision=2 step=0.5></jj-input-number>');
--- packages/jj-slider/tests/jj-slider.stories.js
+++ packages/jj-slider/tests/jj-slider.stories.js
@@ -3,5 +3,6 @@ import {storiesOf} from '@storybook/html';
 import '../jj-slider.js';
 
 storiesOf('jj-slider', module)
-  .add('default', () => 
-      '<jj-input-number min=0 max=8 value=1 precision=2 step=0.5></jj-input-number>');
+  .add('default', () => '<jj-slider></jj-slider>')
+  .add('min, max', () => '<jj-slider min=-100 max=100></jj-slider>')
+  .add('min, max, start value', () => '<jj-slider value=35 min=0 max=50></jj-slider>');
