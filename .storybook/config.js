d379defa0cc4c687c852fe87827efc6e208915db
Nate <nate@objectsecurity.com>
2019-05-23 15:45:21 -0700

    Clean deadcode in storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,6 @@
 import { configure } from '@storybook/html';
 
 // automatically import all files ending in *.stories.js
-//const req = require.context('../src/stories', true, /\.stories\.js$/);
-
 const req = require.context('../packages/', true, /tests\/.*stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
