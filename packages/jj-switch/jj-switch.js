2e21934bc9b1f14b7059709947db0e61e9cb19cf
ShayaP <shayaparsa6@gmail.com>
2019-05-24 13:36:50 -0700

    created skeleton for switch

---
+++ packages/jj-switch/jj-switch.js
@@ -0,0 +1,7 @@
+//this is the template for the switch
+const template = document.createElement('template');
+template.innerHTML = `
+  <link rel="stylesheet" href="jj-switch.css">
+  <div class="jj-switch">
+  </div>
+`;
