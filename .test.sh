ebad63a3aa10fecc69e421c0a11c8d49019748d5
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-29 16:53:27 -0700

    PR test for script

---
+++ .test.sh
@@ -0,0 +1,14 @@
+testcafe --skip-js-errors "saucelabs:Firefox@:Windows 10"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Chrome@:Windows 10"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Firefox@:OS X 10.11"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Chrome@:OS X 10.11"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Safari@:macOS 10.14"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Chrome@67.0:Windows 10"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:Firefox@52.0:Windows 10" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:Firefox@67.0:Windows 7" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:Firefox@52.0:Windows 7" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:Firefox@52.0:macOS 10.12" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:MicrosoftEdge@16.16299:Windows 10" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:Samsung Galaxy S7 Edge GoogleAPI Emulator@8.1" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:iPhone XS Simulator@12.2" packages/*/tests/*_testcafe.js
+testcafe --skip-js-errsrs "saucelabs:iPhone 8 Simulator@12.2" packages/*/tests/*_testcafe.js
