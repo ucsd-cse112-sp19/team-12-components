86e085792fd621ab07d8b899b7a58b73d3690b0c
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-30 14:22:33 -0700

    Add automatic minify script

---
+++ scripts/minify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# must be ran in the root directory of repo
+DIR="./packages/*"
+FILES=`find $DIR -maxdepth 1 -type f -name '*.js' -exec basename {} \;`
+
+for file in $FILES; do
+    filename=${file%.js} # Get the name without .js extension
+    #node ./node_modules/babel-minify/bin/minify.js packages/core-hello/core-hello.js --out-file input.min.js --mangle.keepClassName
+    node ./node_modules/babel-minify/bin/minify.js packages/$filename/$file --out-file src/$file.min.js --mangle.keepClassName
+    echo minified: $filename into src/$filename
+done 
+
+# Add all our new files to be commited
+echo "git add src/"
+git add src/
