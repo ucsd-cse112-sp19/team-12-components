1186a235b3a5ecd207b153bfb00b5cce2c5d4868
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-30 14:59:28 -0700

    Update minify script to be able to run everywhere

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # must be ran in the root directory of repo
-DIR="./packages/*"
+ROOT_DIR=`git rev-parse --show-toplevel`
+DIR="$ROOT_DIR/packages/*"
 FILES=`find $DIR -maxdepth 1 -type f -name '*.js' -exec basename {} \;`
-CAT_FILE="src/jj-components.min.js"
+CAT_FILE="$ROOT_DIR/src/jj-components.min.js"
 
 rm $CAT_FILE
 touch $CAT_FILE
 
 for file in $FILES; do
     filename=${file%.js} # Get the name without .js extension
-    node ./node_modules/babel-minify/bin/minify.js packages/$filename/$file --out-file src/$filename.min.js --mangle.keepClassName
-    cat src/$filename.min.js >> $CAT_FILE
+    node $ROOT_DIR/node_modules/babel-minify/bin/minify.js $ROOT_DIR/packages/$filename/$file --out-file $ROOT_DIR/src/$filename.min.js --mangle.keepClassName
+    cat $ROOT_DIR/src/$filename.min.js >> $CAT_FILE
     echo minified: $file into src/$file.min.js
 done 
 
-echo Building $CAT_FILE
-node ./node_modules/babel-minify/bin/minify.js $CAT_FILE --out-file $CAT_FILE --mangle.keepClassName
+node $ROOT_DIR/node_modules/babel-minify/bin/minify.js $CAT_FILE --out-file $CAT_FILE --mangle.keepClassName
 
 # Add all our new files to be commited
-echo "git add src/"
-git add src/
+echo "git add $ROOT_DIR/src/"
+git add $ROOT_DIR/src/
