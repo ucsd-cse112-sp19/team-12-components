0abf7d9009c3161100925ae80fb78e151cbb019a
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-30 14:39:26 -0700

    Add concatenation script into jj-components.min.js

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -2,13 +2,20 @@
 # must be ran in the root directory of repo
 DIR="./packages/*"
 FILES=`find $DIR -maxdepth 1 -type f -name '*.js' -exec basename {} \;`
+CAT_FILE="src/jj-components.min.js"
+
+rm $CAT_FILE
+touch $CAT_FILE
 
 for file in $FILES; do
     filename=${file%.js} # Get the name without .js extension
-    #node ./node_modules/babel-minify/bin/minify.js packages/core-hello/core-hello.js --out-file input.min.js --mangle.keepClassName
-    node ./node_modules/babel-minify/bin/minify.js packages/$filename/$file --out-file src/$file.min.js --mangle.keepClassName
-    echo minified: $file into src/$file
+    node ./node_modules/babel-minify/bin/minify.js packages/$filename/$file --out-file src/$filename.min.js --mangle.keepClassName
+    cat src/$filename.min.js >> $CAT_FILE
+    echo minified: $file into src/$file.min.js
 done 
+
+echo Building $CAT_FILE
+node ./node_modules/babel-minify/bin/minify.js $CAT_FILE --out-file $CAT_FILE --mangle.keepClassName
 
 # Add all our new files to be commited
 echo "git add src/"
