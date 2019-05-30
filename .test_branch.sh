fe28521dba17d40932924322b97cca57a851c07d
=nonguyen1 <nonguyen@ucsd.edu>
2019-05-29 18:06:36 -0700

    Separate branch and pr testing.

---
+++ .test_branch.sh
@@ -0,0 +1,3 @@
+testcafe --skip-js-errors "saucelabs:Firefox@:Windows 10"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Chrome@:Windows 10"  packages/*/tests/*_testcafe.js
+testcafe --skip-js-errors "saucelabs:Safari@:macOS 10.14"  packages/*/tests/*_testcafe.js
