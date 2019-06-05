ca252caabd76f96f7d1ff1a06284197fb2be8e9e
rkeng <rkeng@ucsd.edu>
2019-06-04 19:01:04 -0700

    added value, label getters and setters

--- packages/jj-checkbox/jj-checkbox.js
+++ packages/jj-checkbox/jj-checkbox.js
@@ -1,7 +1,7 @@
 const JJCheckbox =
-    () => {
-      const template = document.createElement('template');
-      template.innerHTML = `
+  () => {
+    const template = document.createElement('template');
+    template.innerHTML = `
       <style>
           @import url("https://unpkg.com/element-ui/lib/theme-chalk/checkbox.css");
 
@@ -40,81 +40,84 @@ const JJCheckbox =
       </div>
       `;
 
-      class JJCheckbox extends HTMLElement {
-        constructor() {
-          super();
-          this.root = this.attachShadow({mode : 'open'});
-          this.root.appendChild(template.content.cloneNode(true));
+    class JJCheckbox extends HTMLElement {
+      constructor() {
+        super();
+        this.root = this.attachShadow({ mode: 'open' });
+        this.root.appendChild(template.content.cloneNode(true));
 
-          // target elements with querySelector
-          this.checkboxContainer = this.root.querySelector("label.el-checkbox");
-          this.checkboxInputSpan =
-              this.root.querySelector("span.el-checkbox__input");
-          this.labelSpan = this.root.querySelector(".el-checkbox__label span");
+        // target elements with querySelector
+        this.checkboxContainer = this.root.querySelector("label.el-checkbox");
+        this.checkboxInputSpan =
+          this.root.querySelector("span.el-checkbox__input");
+        this.labelSpan = this.root.querySelector(".el-checkbox__label span");
 
-          // Bind "this" to functions to reserve context
-          this.onCheckboxClick = this.onCheckboxClick.bind(this);
+        // Bind "this" to functions to reserve context
+        this.onCheckboxClick = this.onCheckboxClick.bind(this);
+      }
+
+      connectedCallback() {
+        // Bind event listener to checkbox elements
+        this.checkboxContainer.addEventListener('mousedown',
+          this.onCheckboxClick);
+
+        if (this.hasAttribute('value')) {
+          this._value = this.getAttribute('value');
+        } else {
+          this._value = "option value";
         }
 
-        connectedCallback() {
-          // Bind event listener to checkbox elements
-          this.checkboxContainer.addEventListener('mousedown',
-                                                  this.onCheckboxClick);
+        if (this.hasAttribute('label')) {
+          this.label = this.getAttribute('label');
+          this.labelSpan.innerHTML = this.label;
+        } else {
+          this.label = "Option";
+          this.labelSpan.innerHTML = this.label;
+        }
 
-          if (this.hasAttribute('value')) {
-            this._value = this.getAttribute('value');
-          } else {
-            this._value = "option value";
-          }
+        if (this.hasAttribute('checked')) {
+          this.checked = true;
+          this.checkboxContainer.classList.add("is-checked");
+          this.checkboxInputSpan.classList.add("is-checked");
+        } else {
+          this.checked = false;
+        }
 
-          if (this.hasAttribute('label')) {
-            this.label = this.getAttribute('label');
-            this.labelSpan.innerHTML = this.label;
-          } else {
-            this.label = "Option";
-            this.labelSpan.innerHTML = this.label;
-          }
+        if (this.hasAttribute('disabled')) {
+          this.disabled = true;
+          this.checkboxContainer.classList.add("is-disabled");
+          this.checkboxInputSpan.classList.add("is-disabled");
+        } else {
+          this.disabled = false;
+        }
+      }
 
-          if (this.hasAttribute('checked')) {
-            this.checked = true;
+      onCheckboxClick(event) {
+        if (!this.disabled) {
+          this.checked = !this.checked;
+          console.log(this.checked ? "Checked " + this._value
+            : "Unchecked " + this._value);
+          if (this.checked) {
+            this.setAttribute("checked", "");
             this.checkboxContainer.classList.add("is-checked");
             this.checkboxInputSpan.classList.add("is-checked");
           } else {
-            this.checked = false;
-          }
-
-          if (this.hasAttribute('disabled')) {
-            this.disabled = true;
-            this.checkboxContainer.classList.add("is-disabled");
-            this.checkboxInputSpan.classList.add("is-disabled");
-          } else {
-            this.disabled = false;
+            this.removeAttribute("checked");
+            this.checkboxContainer.classList.remove("is-checked");
+            this.checkboxInputSpan.classList.remove("is-checked");
           }
         }
+      }
 
-        onCheckboxClick(event) {
-          if (!this.disabled) {
-            this.checked = !this.checked;
-            console.log(this.checked ? "Checked " + this._value
-                                     : "Unchecked " + this._value);
-            if (this.checked) {
-              this.setAttribute("checked", "");
-              this.checkboxContainer.classList.add("is-checked");
-              this.checkboxInputSpan.classList.add("is-checked");
-            } else {
-              this.removeAttribute("checked");
-              this.checkboxContainer.classList.remove("is-checked");
-              this.checkboxInputSpan.classList.remove("is-checked");
-            }
-          }
-        }
+      // Observe only the array of attribute names
+      static get observedAttributes() { return ['label', 'checked', 'disabled']; }
 
-        // Observe only the array of attribute names
-        static get observedAttributes() { return [ 'checked', 'disabled' ]; }
-
-        // Listen for changed attributes
-        attributeChangedCallback(name, oldValue, newValue) {
-          switch (name) {
+      // Listen for changed attributes
+      attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+          case 'label':
+            this.labelSpan.innerHTML = newValue;
+            break;
           case 'checked':
             if (newValue !== null) {
               this.checkboxContainer.classList.add("is-checked");
@@ -133,31 +136,39 @@ const JJCheckbox =
               this.checkboxInputSpan.classList.remove("is-disabled");
             }
             break;
-          }
-        }
-
-        // Getters
-        get checked() { return this.hasAttribute('checked'); }
-        get disabled() { return this.hasAttribute('disabled'); }
-
-        // Setters
-        set checked(newValue) {
-          if (newValue) {
-            this.setAttribute('checked', "");
-          } else {
-            this.removeAttribute('checked');
-          }
-        }
-        set disabled(newValue) {
-          if (newValue) {
-            this.setAttribute('disabled', "");
-          } else {
-            this.removeAttribute('disabled');
-          }
         }
       }
 
-      customElements.define('jj-checkbox', JJCheckbox);
+      // Getters
+      get value() { return this.getAttribute('value'); }
+      get label() { return this.getAttribute('label'); }
+      get checked() { return this.hasAttribute('checked'); }
+      get disabled() { return this.hasAttribute('disabled'); }
+
+      // Setters
+      set value(newValue) {
+        this.setAttribute('value', newValue);
+      }
+      set label(newValue) {
+        this.setAttribute('label', newValue);
+      }
+      set checked(newValue) {
+        if (newValue) {
+          this.setAttribute('checked', "");
+        } else {
+          this.removeAttribute('checked');
+        }
+      }
+      set disabled(newValue) {
+        if (newValue) {
+          this.setAttribute('disabled', "");
+        } else {
+          this.removeAttribute('disabled');
+        }
+      }
     }
+
+    customElements.define('jj-checkbox', JJCheckbox);
+  }
 
 JJCheckbox();
