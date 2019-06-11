6e56effe62b2c0ce18dcaedd7fb9150d6795be35
rkeng <rkeng@ucsd.edu>
2019-06-11 14:54:59 -0700

    refactored jj-button for maintainability score of A

--- packages/jj-button/jj-button.js
+++ packages/jj-button/jj-button.js
@@ -15,9 +15,8 @@
  */
 
 //define the css for this component
-const jjButton = () => {
-  const template = document.createElement('template');
-  template.innerHTML = `
+const jjButtonTemplate = document.createElement('template');
+jjButtonTemplate.innerHTML = `
   <style>
     .btn {
       display: inline-block;
@@ -41,35 +40,28 @@ const jjButton = () => {
       font-size: 14px;
       border-radius: 4px;
     }
-    .btn:focus,
-    .btn:hover {
+    .btn:focus, .btn:hover {
       color:#409eff;
       border-color:#c6e2ff;
       background-color:#ecf5ff
     }
-
-    .btn.plain:focus,
-    .btn.plain:hover {
+    .btn.plain:focus, .btn.plain:hover {
       background:#fff;
       border-color:#409eff;
       color:#409eff
     }
-
     .btn.round {
       border-radius:20px;
       padding:12px 23px
     }
-
     .btn.circle {
       border-radius:50%;
       padding:12px
     }
-
     .btn.disabled {
       opacity: 0.4;
       pointer-events: none;
     }
-
     .btn.text {
       border-color:transparent;
       color:#409eff;
@@ -77,8 +69,7 @@ const jjButton = () => {
       padding-left:0;
       padding-right:0
     }
-    .btn.text:focus,
-    .btn.text:hover {
+    .btn.text:focus, .btn.text:hover {
       color:#66b1ff;
       border-color:transparent;
       background-color:transparent
@@ -87,7 +78,6 @@ const jjButton = () => {
       color:#3a8ee6;
       background-color:transparent
     }
-
     .btn.medium {
       padding:10px 20px;
       font-size:14px;
@@ -140,21 +130,17 @@ const jjButton = () => {
       background-color: #409eff;
       border-color: #409eff;
     }
-
-    .primary:focus,
-    .primary:hover {
+    .primary:focus, .primary:hover {
       background:#66b1ff;
       border-color:#66b1ff;
       color:#fff
     }
-
     .primary.plain {
       color:#409eff;
       background:#ecf5ff;
       border-color:#b3d8ff
     }
-    .primary.plain:focus,
-    .primary.plain:hover {
+    .primary.plain:focus, .primary.plain:hover {
       background:#409eff;
       border-color:#409eff;
       color:#fff
@@ -171,21 +157,17 @@ const jjButton = () => {
       background-color: #67c23a;
       border-color: #67c23a;
     }
-
-    .success:focus,
-    .success:hover {
+    .success:focus, .success:hover {
       background:#85ce61;
       border-color:#85ce61;
       color:#fff
     }
-
     .success.plain {
       color:#67c23a;
       background:#f0f9eb;
       border-color:#c2e7b0
     }
-    .success.plain:focus,
-    .success.plain:hover {
+    .success.plain:focus, .success.plain:hover {
       background:#67c23a;
       border-color:#67c23a;
       color:#fff
@@ -202,21 +184,17 @@ const jjButton = () => {
       background-color: #909399;
       border-color: #909399;
     }
-
-    .info:focus,
-    .info:hover {
+    .info:focus, .info:hover {
       background:#a6a9ad;
       border-color:#a6a9ad;
       color:#fff
     }
-
     .info.plain {
       color:#909399;
       background:#f4f4f5;
       border-color:#d3d4d6
     }
-    .info.plain:focus,
-    .info.plain:hover {
+    .info.plain:focus, .info.plain:hover {
       background:#909399;
       border-color:#909399;
       color:#fff
@@ -233,21 +211,17 @@ const jjButton = () => {
       background-color: #e6a23c;
       border-color: #e6a23c;
     }
-
-    .warning:focus,
-    .warning:hover {
+    .warning:focus, .warning:hover {
       background:#ebb563;
       border-color:#ebb563;
       color:#fff
     }
-
     .warning.plain {
       color:#e6a23c;
       background:#fdf6ec;
       border-color:#f5dab1
     }
-    .warning.plain:focus,
-    .warning.plain:hover {
+    .warning.plain:focus, .warning.plain:hover {
       background:#e6a23c;
       border-color:#e6a23c;
       color:#fff
@@ -264,21 +238,17 @@ const jjButton = () => {
       background-color: #f56c6c;
       border-color: #f56c6c;
     }
-
-    .danger:focus,
-    .danger:hover {
+    .danger:focus, .danger:hover {
       background:#f78989;
       border-color:#f78989;
       color:#fff
     }
-
     .danger.plain {
       color:#f56c6c;
       background:#fef0f0;
       border-color:#fbc4c4
     }
-    .danger.plain:focus,
-    .danger.plain:hover {
+    .danger.plain:focus, .danger.plain:hover {
       background:#f56c6c;
       border-color:#f56c6c;
       color:#fff
@@ -289,172 +259,148 @@ const jjButton = () => {
       color:#fff;
       outline:none
     }
-
   </style>
   <button id="jj-button" class="btn"><slot></slot></button> 
   `;
 
-  class JJButton extends HTMLElement {
-    static get observedAttributes() {
-      return [
-        'size', 'type', 'round', 'plain', 'circle', 'loading', 'disabled', 'icon', 'autofocus', 'native-type'
-      ];
-    }
-    constructor () {
-      super();
-      this.root = this.attachShadow({mode : 'open'});
-      this.root.appendChild(template.content.cloneNode(true));
-
-      //define the elements of this component
-      this.button = this.root.querySelector('.btn');
-      
-    }
-
-    connectedCallback() {
-      //set the round attribute
-      if (this.hasAttribute('round')) {
-        if (this.getAttribute('round') == 'true') {
-          this.round = "true";
-          this.button.classList.add('round');
-        } else {
-          this.round = "false";
-        }
-      } else {
-        this.round = "false";
-      }
-
-      //set the plain attribute
-      if (this.hasAttribute('plain')) {
-        if (this.getAttribute('plain') == 'true') {
-          this.plain = "true";
-          this.button.classList.add('plain');
-        } else {
-          this.plain = "false";
-        }
-      } else {
-        this.plain = "false";
-      }
-
-      //set the circle attribute
-      if (this.hasAttribute('circle')) {
-        if (this.getAttribute('circle') == 'true') {
-          this.button.classList.add('circle');
-          this.circle = "true";
-        } else {
-          this.cicle = "false";
-        }
-      } else {
-        this.circle = "false";
-      }
-
-      // set the size attribute
-      if (this.hasAttribute('size')) {
-        this.size = this.getAttribute('size');
-        this.button.classList.add(this.size);
-      }
-
-      // set the type attribute
-      if (this.hasAttribute('type')) {
-        this.type = this.getAttribute('type');
-        this.button.classList.add(this.type);
-      } else {
-        this.type = "default";
-      }
-      // set the disabled attribute
-      if (this.hasAttribute('disabled')) {
-        if (this.getAttribute('disabled') == 'true') {
-          this.disabled = true;
-          this.button.classList.add('disabled')
-        } else {
-          this.disabled = false;
-        }
-      } else {
-        this.disabled = false;
-      }
-
-      // set the native type attribute
-      if (this.hasAttribute('native-type')) {
-        this.native_type = this.getAttribute('native-type');
-        this.button.setAttribute('type', this.getAttribute('native-type'));
-      } else {
-        this.native_type="button";
-      }
-
-      // set the autofocus attribute
-      if (this.hasAttribute('autofocus')) {
-        this.autofocus = this.getAttribute('autofocus');
-        this.button.setAttribute('autofocus', this.getAttribute('autofocus'));
-      } else {
-        this.autofocus="false";
-      }
-    }
-
-    attributeChangedCallback(attrName, oldValue, newValue) {
-      switch (attrName) {
-        case 'size':
-          this.button.classList.remove(oldValue);
-          this.button.classList.add(newValue);
-          break;
-        case 'type':
-          this.button.classList.remove(oldValue);
-          this.button.classList.add(newValue);
-          break;
-        case 'disabled':
-          if (newValue == "false") {
-            this.button.classList.remove('disabled');
-          } else {
-            this.button.classList.add('disabled');
-          }
-          break;
-        case 'circle':
-          if (newValue == "false") {
-            this.button.classList.remove('circle');
-          } else {
-            this.button.classList.add('circle');
-          }
-          break;
-        case 'plain':
-          if (newValue == "false") {
-            this.button.classList.remove('plain');
-          } else {
-            this.button.classList.add('plain');
-          }
-          break;
-        case 'round':
-          if (newValue == "false") {
-            this.button.classList.remove('round');
-          } else {
-            this.button.classList.add('round');
-          }
-          break;
-        case 'autofocus':
-          this.button.setAttribute('autofocus', newValue);
-          break;
-        case 'native-type':
-          this.button.setAttribute('type', newValue);
-      }
-    }
-
-    //Getters
-    get round() {return this.getAttribute('round') == 'true';}
-    get plain() {return this.getAttribute('plain') == 'true';}
-    get circle() {return this.getAttribute('circle') == 'true';}
-    get size() {return this.getAttribute('size');}
-    get disabled() {return this.getAttribute('disabled');}
-    get type() {return this.getAttribute('type');}
-    get native_type() {return this.getAttribute('native-type');}
-    get autofocus() {return this.getAttribute('autofocus');}
-
-    //Setters
-    set round(newValue) {this.setAttribute('round', newValue);}
-    set plain(newValue) {this.setAttribute('plain', newValue);}
-    set circle(newValue) {this.setAttribute('circle', newValue);}
-    set size(newValue) {this.setAttribute('size', newValue);}
-    set disabled(newValue) {this.setAttribute('disabled', newValue);}
-    set type(newValue) {this.setAttribute('type', newValue);}
-    set native_type(newValue) {this.setAttribute('native-type', newValue);}
-    set autofocus(newValue) {this.setAttribute('autofocus', newValue);}
+class JJButton extends HTMLElement {
+  static get observedAttributes() {
+    return [
+      'size', 'type', 'round', 'plain', 'circle', 'loading', 'disabled', 'icon', 'autofocus', 'native-type'
+    ];
   }
-  customElements.define('jj-button', JJButton);
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: 'open' });
+    this.root.appendChild(jjButtonTemplate.content.cloneNode(true));
+
+    //define the elements of this component
+    this.button = this.root.querySelector('.btn');
+
+    // Bind "this" to functions to reserve context
+    this.initPhysProps = this.initPhysProps.bind(this);
+    this.initSizeType = this.initSizeType.bind(this);
+    this.initNativeAutofocus = this.initNativeAutofocus.bind(this);
+    this.classNameSwitch = this.classNameSwitch.bind(this);
+  }
+
+  // helper functions for connectedCallback
+  initPhysProps(attr) {
+    if (this.hasAttribute(attr) && this.getAttribute(attr) == 'true') {
+      this[attr] = 'true';
+      this.button.classList.add(attr);
+    } else {
+      this[attr] = 'false';
+    }
+  }
+  initSizeType(attr) {
+    this[attr] = this.getAttribute(attr);
+    this.button.classList.add(this[attr]);
+  }
+  initNativeAutofocus(attr) {
+    let newAttr = attr.replace(/-/g, "_");
+    this[newAttr] = this.getAttribute(newAttr);
+    this.button.setAttribute(newAttr, this.getAttribute(newAttr));
+  }
+
+  // Helper function for attributeChangedCallback
+  classNameSwitch(attr, value) {
+    if (value == "false") {
+      this.button.classList.remove(attr);
+    } else {
+      this.button.classList.add(attr);
+    }
+  }
+
+  connectedCallback() {
+    // set the round attribute
+    this.initPhysProps('round');
+
+    // set the plain attribute
+    this.initPhysProps('plain');
+
+    // set the circle attribute
+    this.initPhysProps('circle');
+
+    // set the disabled attribute
+    this.initPhysProps('disabled');
+
+    // set the size attribute
+    if (this.hasAttribute('size')) {
+      this.initSizeType('size');
+    }
+
+    // set the type attribute
+    if (this.hasAttribute('type')) {
+      this.initSizeType('type');
+    } else {
+      this.type = "default";
+    }
+
+    // set the native type attribute
+    if (this.hasAttribute('native-type')) {
+      this.initNativeAutofocus('native-type');
+    } else {
+      this.native_type = 'button';
+    }
+
+    // set the autofocus attribute
+    if (this.hasAttribute('autofocus')) {
+      this.initNativeAutofocus('autofocus');
+    } else {
+      this.autofocus = 'false';
+    }
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    switch (attrName) {
+      case 'size':
+        this.button.classList.remove(oldValue);
+        this.button.classList.add(newValue);
+        break;
+      case 'type':
+        this.button.classList.remove(oldValue);
+        this.button.classList.add(newValue);
+        break;
+      case 'disabled':
+        this.classNameSwitch('disabled', newValue);
+        break;
+      case 'circle':
+        this.classNameSwitch('circle', newValue);
+        break;
+      case 'plain':
+        this.classNameSwitch('plain', newValue);
+        break;
+      case 'round':
+        this.classNameSwitch('round', newValue);
+        break;
+      case 'autofocus':
+        this.button.setAttribute('autofocus', newValue);
+        break;
+      case 'native-type':
+        this.button.setAttribute('type', newValue);
+        break;
+    }
+  }
+
+  //Getters
+  get round() { return this.getAttribute('round') == 'true'; }
+  get plain() { return this.getAttribute('plain') == 'true'; }
+  get circle() { return this.getAttribute('circle') == 'true'; }
+  get size() { return this.getAttribute('size'); }
+  get disabled() { return this.getAttribute('disabled'); }
+  get type() { return this.getAttribute('type'); }
+  get native_type() { return this.getAttribute('native-type'); }
+  get autofocus() { return this.getAttribute('autofocus'); }
+
+  //Setters
+  set round(newValue) { this.setAttribute('round', newValue); }
+  set plain(newValue) { this.setAttribute('plain', newValue); }
+  set circle(newValue) { this.setAttribute('circle', newValue); }
+  set size(newValue) { this.setAttribute('size', newValue); }
+  set disabled(newValue) { this.setAttribute('disabled', newValue); }
+  set type(newValue) { this.setAttribute('type', newValue); }
+  set native_type(newValue) { this.setAttribute('native-type', newValue); }
+  set autofocus(newValue) { this.setAttribute('autofocus', newValue); }
 }
-jjButton();
+customElements.define('jj-button', JJButton);
