bb3cb3fd03f20b17dd4e6dbcce9e6ee2322a9599
Chris Synnott <csynnott@ucsd.edu>
2019-06-11 19:15:46 -0700

    fixed json build error. Builds guides properly

--- packages/jj-button/jj-button.js
+++ packages/jj-button/jj-button.js
@@ -1,6 +1,6 @@
 /**
- * @attribute_name jj button
- * @attribute_desc A simple button with various attributes and styling.
+ * @component_name jj button
+ * @component_desc A simple button with various attributes and styling.
  * @attribute size, button size, string, medium/small/mini, /
  * @attribute type, button type, string, primary/success/warning/danger/info/ text, /
  * @attribute plain, determine whether it's a plain button, boolean, /, false
