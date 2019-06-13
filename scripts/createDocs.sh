77e89135d070ff48e65aa73071839bec745ceb23
nonguyen1 <nonguyen@ucsd.edu>
2019-06-13 15:56:41 -0700

    Add doc create script

---
+++ scripts/createDocs.sh
@@ -0,0 +1,7 @@
+rm -r ./docs/en/docs/components
+rm -r ./docs/es/docs/components
+rm -r ./docs/fr/docs/components
+rm -r ./docs/zh/docs/components
+node createDocs.js ./packages .js
+#gulp compileComponents --option production
+gulp compileComponents --option dev
